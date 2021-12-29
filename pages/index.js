d224cc4e552d872ac1c09468a902725cc78d8ed0
Skunpoj Thanarojsophon <kimgiftww@gmail.com>
2021-12-29 22:14:55 +0700

    1

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function PrivatePage(props) {
     <div>
       <div>
         <img src={createObjectURL} />
-        <h4>Select Image</h4>
+        <h4>Select Image V2</h4>
         <input type="file" name="myImage" onChange={uploadToClient} />
         <button
           className="btn btn-primary"
